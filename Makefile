007e6cb7e4e69164d56db160f0664bfd3fb9de89
brian <rbrianhazzard@gmail.com>
2025-08-11 18:45:14 -0400

    fixes

--- Makefile
+++ Makefile
@@ -13,9 +13,10 @@ help:
 	@grep '^.PHONY: ' Makefile | sed 's/.PHONY: //' | awk '{split($$0,a," ## "); printf "  \033[34m%0-10s\033[0m %s\n", a[1], a[2]}'
 
 
-.PHONY: clean ## Deletes local mod folder
+.PHONY: clean ## Deletes local mod folder and local packages
 clean:
 	@rm -rf "$(MOD_FOLDER_LOC)/Ci6ndex"
+	@rm -rf build/
 
 .PHONY: dev ## Copies script files to local Civ 6 installation for debugging / testing
 dev:
@@ -28,4 +29,11 @@ build:
 	@mkdir -p $(DEV_TOOLS_LOC)/Ci6ndex
 	@rm -rf $(DEV_TOOLS_LOC)/Ci6ndex/*
 	@cp -r * $(DEV_TOOLS_LOC)/Ci6ndex/
-    
+
+.PHONY: package ## Creates a tar archive of the mod
+package:
+	@mkdir -p build
+	@mkdir -p build/temp/Ci6ndex
+	@cp -r $(filter-out build/%, $(wildcard *)) build/temp/Ci6ndex/
+	@tar -czf build/ci6ndex.tar.gz -C build/temp --exclude=.git --exclude=.gitignore .
+	@rm -rf build/temp
--- sql/civdex_strat_resources.sql
+++ sql/civdex_strat_resources.sql
@@ -1,5 +1,6 @@
 INSERT INTO RequirementSets (RequirementSetId, RequirementSetType)
 VALUES
+    ('PLAYER_CAN_SEE_NITER_CIVDEX'	, 	'REQUIREMENTSET_TEST_ALL'),
     ('PLAYER_CAN_SEE_ALUMINUM_CIVDEX'	, 	'REQUIREMENTSET_TEST_ALL'),
     ('PLAYER_CAN_SEE_COAL_CIVDEX'	, 	'REQUIREMENTSET_TEST_ALL'),
     ('PLAYER_CAN_SEE_OIL_CIVDEX'		, 	'REQUIREMENTSET_TEST_ALL');
@@ -7,18 +8,29 @@ VALUES
 -- only trigger when player can see resource, even if built before
 INSERT INTO RequirementSetRequirements (RequirementSetId, RequirementId)
 VALUES
+    ('PLAYER_CAN_SEE_NITER_CIVDEX'	, 'REQUIRES_PLAYER_CAN_SEE_NITER'),
     ('PLAYER_CAN_SEE_ALUMINUM_CIVDEX'	, 'REQUIRES_PLAYER_CAN_SEE_ALUMINUM'),
     ('PLAYER_CAN_SEE_COAL_CIVDEX'	, 'REQUIRES_PLAYER_CAN_SEE_COAL'),
     ('PLAYER_CAN_SEE_OIL_CIVDEX'		, 'REQUIRES_PLAYER_CAN_SEE_OIL');
 
--- +1 coal for seaports
+-- +1 niter for armory
 INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
-    ('BUILDING_SEAPORT', 'COAL_FROM_SEAPORT_CIVDEX');
+    ('BUILDING_ARMORY', 'NITER_FROM_ARMORY_CIVDEX');
 INSERT INTO Modifiers (ModifierId, ModifierType, SubjectRequirementSetId) VALUES
-    ('COAL_FROM_SEAPORT_CIVDEX', 'MODIFIER_PLAYER_ADJUST_FREE_RESOURCE_IMPORT_EXTRACTION', 'PLAYER_CAN_SEE_COAL_CIVDEX');
+    ('NITER_FROM_ARMORY_CIVDEX', 'MODIFIER_PLAYER_ADJUST_FREE_RESOURCE_IMPORT_EXTRACTION', 'PLAYER_CAN_SEE_NITER_CIVDEX');
 INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
-    ('COAL_FROM_SEAPORT_CIVDEX', 'ResourceType', 'RESOURCE_COAL'),
-    ('COAL_FROM_SEAPORT_CIVDEX', 'Amount', '1');
+    ('NITER_FROM_ARMORY_CIVDEX', 'ResourceType', 'RESOURCE_NITER'),
+    ('NITER_FROM_ARMORY_CIVDEX', 'Amount', '1');
+
+-- +1 coal for shipyards
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+    ('BUILDING_SHIPYARD', 'COAL_FROM_SHIPYARD_CIVDEX');
+INSERT INTO Modifiers (ModifierId, ModifierType, SubjectRequirementSetId) VALUES
+    ('COAL_FROM_SHIPYARD_CIVDEX', 'MODIFIER_PLAYER_ADJUST_FREE_RESOURCE_IMPORT_EXTRACTION', 'PLAYER_CAN_SEE_COAL_CIVDEX');
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
+    ('COAL_FROM_SHIPYARD_CIVDEX', 'ResourceType', 'RESOURCE_COAL'),
+    ('COAL_FROM_SHIPYARD_CIVDEX', 'Amount', '1');
+
 -- +1 oil from mil acadamies
 INSERT INTO BuildingModifiers (BuildingType, ModifierId) 
 VALUES
@@ -31,21 +43,14 @@ VALUES
     ('OIL_FROM_MIL_ACAD_CIVDEX', 'ResourceType', 'RESOURCE_OIL'),
     ('OIL_FROM_MIL_ACAD_CIVDEX', 'Amount', '1');
 
--- +1 alum from airports
+-- +1 alum from hangars
 INSERT INTO BuildingModifiers (BuildingType, ModifierId) 
 VALUES
-    ('BUILDING_AIRPORT', 'ALUM_FROM_AIRPORT_CIVDEX');
+    ('BUILDING_HANGAR', 'ALUM_FROM_HANGAR_CIVDEX');
 INSERT INTO Modifiers (ModifierId, ModifierType, SubjectRequirementSetId) 
 VALUES
-    ('ALUM_FROM_AIRPORT_CIVDEX', 'MODIFIER_PLAYER_ADJUST_FREE_RESOURCE_IMPORT_EXTRACTION', 'PLAYER_CAN_SEE_ALUMINUM_CIVDEX');
+    ('ALUM_FROM_HANGAR_CIVDEX', 'MODIFIER_PLAYER_ADJUST_FREE_RESOURCE_IMPORT_EXTRACTION', 'PLAYER_CAN_SEE_ALUMINUM_CIVDEX');
 INSERT INTO ModifierArguments (ModifierId, Name, Value)
 VALUES
-    ('ALUM_FROM_AIRPORT_CIVDEX', 'ResourceType', 'RESOURCE_ALUMINUM'),
-    ('ALUM_FROM_AIRPORT_CIVDEX', 'Amount', '1');
-
--- 
--- [766238.837] [Gameplay]: Validating Foreign Key Constraints...
--- [766238.845] [Gameplay] ERROR: Invalid Reference on Modifiers.SubjectRequirementSetId - "PLAYER_CAN_SEE_OIL_CIVDEX" does not exist in RequirementSets
--- [766238.845] [Gameplay] ERROR: Invalid Reference on Modifiers.SubjectRequirementSetId - "PLAYER_CAN_SEE_ALUMINUM_CIVDEX" does not exist in RequirementSets
--- [766238.849] [Gameplay] ERROR: Invalid Reference on RequirementSetRequirements.RequirementSetId - "PLAYER_CAN_SEE_ALUMINUM_CIVDEX" does not exist in RequirementSets
--- [766238.849] [Gameplay] ERROR: Invalid Reference on RequirementSetRequirements.RequirementSetId - "PLAYER_CAN_SEE_OIL_CIVDEX" does not exist in RequirementSets
+    ('ALUM_FROM_HANGAR_CIVDEX', 'ResourceType', 'RESOURCE_ALUMINUM'),
+    ('ALUM_FROM_HANGAR_CIVDEX', 'Amount', '1');
